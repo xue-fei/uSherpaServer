c74cf15ffbda2bb9b215675a0831801c903cfada
xue-fei <awnuxcvbn@163.com>
2024-04-17 10:39:26 +0800

    添加包依赖

--- Program.cs
+++ Program.cs
@@ -1,2 +1,17 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+using System;
+
+namespace uSherpaServer
+{
+    internal class Program
+    {
+        // 声明配置和识别器变量
+        private SherpaNcnn.OnlineRecognizer recognizer;
+        private SherpaNcnn.OnlineStream onlineStream;
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
